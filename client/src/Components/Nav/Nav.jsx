753dc11816dde52fae62e7a9511c3a6d924769a7
DS Rathore <dsrathorebsw1234@gmail.com>
2022-11-17 09:09:43 +0530

    Remove Unwanted Links Tag"

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './NavStyle.css';
 
 const Nav = () => {
     return (
         <>
             <div className='container' id='nav'>
-                <span className='logo'> DS<br/><span className='s'>RA</span></span>
+                <span className='logo'> DS<br /><span className='s'>RA</span></span>
                 <div className='MiddleTitle'>Portfo<span>lio.</span></div>
                 <div>
-                    <Link to='/about'>
-                        <a href='#about' className='aboutBtn' >About me</a>
-                    </Link>
-                    <Link to='/contact'>
-                        <a href='#contactMe' className='contactBtn btn'>Donate me</a>
-                    </Link>
+                    <a href='#about' className='aboutBtn' >About me</a>
+                    <a href='#contactMe' className='donateBtn btn'>Donate me</a>
                 </div>
             </div>
         </>
