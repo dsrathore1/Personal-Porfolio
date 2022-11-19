c5bc189039e5411e826cbefefab3353b1dcbdd66
DS Rathore <dsrathorebsw1234@gmail.com>
2022-11-19 05:34:36 +0530

    New Commit

--- client/src/Pages/Projects/ProjectFolder.jsx
+++ client/src/Pages/Projects/ProjectFolder.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import ProjectFolderTile from '../../Components/ProjectFolderTile/ProjectFolderTile';
 import './ProjectFolderStyle.css';
 
 const ProjectFolder = () => {
     return (
         <>
-            <ProjectFolderTile />
         </>
     )
 }
